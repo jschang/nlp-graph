f1a2f1c1c27fc4c27a2476ebce8d56cf2acdd2ef
Ubuntu <ubuntu@ip-10-102-182-112.ec2.internal>
2015-07-22 17:15:17 +0000

    adding k520 driver install convenience script

---
+++ install_grid-k520_driver.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo apt-get install inux-headers-generic
+wget http://us.download.nvidia.com/XFree86/Linux-x86_64/340.46/NVIDIA-Linux-x86_64-340.46.run
+sudo bash ./NVIDIA-Linux-x86_64-340.46.run
