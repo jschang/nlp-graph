a394c62251c69cb92fa34b8858ce2c86c4d01cb7
Jonathan Schang <schang@Jonathans-MacBook-Pro.local>
2016-02-29 21:32:43 -0800

    stubbing out smith-waterman

---
+++ NLPGraph/calc/smith_waterman.cpp
@@ -0,0 +1,9 @@
+//
+//  smith_waterman.cpp
+//  NLPGraph
+//
+//  Created by Jonathan Schang on 2/29/16.
+//
+//
+
+#include "smith_waterman.hpp"
---
+++ NLPGraph/calc/smith_waterman.h
@@ -0,0 +1,14 @@
+//
+//  smith_waterman.hpp
+//  NLPGraph
+//
+//  Created by Jonathan Schang on 2/29/16.
+//
+//
+
+#ifndef smith_waterman_hpp
+#define smith_waterman_hpp
+
+#include <stdio.h>
+
+#endif /* smith_waterman_hpp */
